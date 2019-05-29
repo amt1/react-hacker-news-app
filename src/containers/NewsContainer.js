43542e99965cb8ba321ed6b2edf62f6a7b29d5bc
amt1 <atreshansky@googlemail.com>
2019-05-29 12:38:29 +0100

    fetching an array of empty promises

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -15,5 +15,24 @@ class NewsContainer extends Component {
     );
   }
 
+  componentDidMount(){
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then((res) => res.json())
+      .then((res) => {
+        const urls = res.map((id) => {
+            return `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+        })
+        Promise.all(urls.map((url) => {
+          return fetch(url);
+        }))
+        .then((res) => {
+          return res.map((res1) => {
+            return res1.json();
+          })
+        })
+        .then((res) => {this.setState({news: res})})
+      })
+
+      }
 }
 export default NewsContainer;
